fc9021f7c195279e78e58a4096ed4eb79d5b472c
Antoine Zanuttini <lezanu@gmail.com>
2018-01-26 23:51:40 +0100

    First interaction

---
+++ GGJ2018/Assets/Scripts/InputSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSystem : MonoBehaviour {
+
+    public List<string> Verbs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AcceptInput() {
+        Debug.Log("Hello");
+    }
+}
